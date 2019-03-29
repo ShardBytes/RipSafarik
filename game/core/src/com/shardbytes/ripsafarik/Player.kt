cf2c80ab6ffe5bb0cf41eb220da87a07eff50e83
Plajdo <31418257+Plajdo@users.noreply.github.com>
2019-03-29 16:05:35 +0100

    synccc

---
+++ game/core/src/com/shardbytes/ripsafarik/Player.kt
@@ -0,0 +1,19 @@
+package com.shardbytes.ripsafarik
+
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.Sprite
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.math.Vector2
+
+class Player : ILockable, ITickable{
+
+    override val position: Vector2 = Vector2(0f, 0f)
+    
+    val playerSprite = Sprite(Texture("android/assets/badlogic.jpg"))
+
+    override fun tick(batch: SpriteBatch, deltaTime: Float) {
+        playerSprite.draw(batch)
+        
+    }
+    
+}
