35e57808607da0ef92a668c5a4a7f232c92f8d83
Plasmoxy <sebohacker@gmail.com>
2019-03-29 14:57:05 +0100

    Create SebuScreen.kt

---
+++ game/core/src/com/shardbytes/ripsafarik/SebuScreen.kt
@@ -0,0 +1,32 @@
+package com.shardbytes.ripsafarik
+
+import com.badlogic.gdx.Screen
+
+class SebuScreen : Screen {
+	
+	
+	
+	override fun hide() {
+	
+	}
+	
+	override fun show() {
+	}
+	
+	override fun render(delta: Float) {
+	}
+	
+	override fun pause() {
+	}
+	
+	override fun resume() {
+	}
+	
+	override fun resize(width: Int, height: Int) {
+	}
+	
+	override fun dispose() {
+	}
+	
+	
+}
