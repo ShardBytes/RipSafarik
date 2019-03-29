f706eaca5b2dca8750cced2542f9eee570b13a97
Plajdo <31418257+Plajdo@users.noreply.github.com>
2019-03-29 14:57:36 +0100

    loadsebuscreen

--- game/core/src/com/shardbytes/ripsafarik/MainGame.kt
+++ game/core/src/com/shardbytes/ripsafarik/MainGame.kt
@@ -1,31 +1,23 @@
 package com.shardbytes.ripsafarik
 
-import com.badlogic.gdx.ApplicationAdapter
-import com.badlogic.gdx.Gdx
-import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.Game
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 
-class MainGame : ApplicationAdapter() {
+class MainGame : Game() {
 	
 	lateinit var batch: SpriteBatch
-	lateinit var img: Texture
 	
 	override fun create() {
 		batch = SpriteBatch()
-		img = Texture("badlogic.jpg")
-	}
-	
-	override fun render() {
-		Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
-		batch.begin()
-		batch.draw(img, 0f, 0f)
-		batch.end()
+		
+		this.setScreen(SebuScreen())
+		
 	}
 	
 	override fun dispose() {
 		batch.dispose()
-		img.dispose()
+		
 	}
+	
 }
