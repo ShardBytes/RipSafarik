2289bb5b1f6a56098a8f148d787ab8e458f8bec3
Plajdo <31418257+Plajdo@users.noreply.github.com>
2019-03-29 17:03:10 +0100

    hhh

--- game/core/src/com/shardbytes/ripsafarik/GameScreen.kt
+++ game/core/src/com/shardbytes/ripsafarik/GameScreen.kt
@@ -14,6 +14,7 @@ class GameScreen : Screen {
     
     init {
         gameObjects.add(Player())
+        gameObjects.add(World())
         
     }
     
--- game/core/src/com/shardbytes/ripsafarik/World.kt
+++ game/core/src/com/shardbytes/ripsafarik/World.kt
@@ -1,7 +1,31 @@
 package com.shardbytes.ripsafarik
 
-class World {
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+
+class World : ITickable {
     
+    val tileMap = arrayOf(
+            arrayOf(0, 0, 0, 0, 0),
+            arrayOf(1, 1, 1, 1, 1),
+            arrayOf(0, 0, 0, 0, 0)
+    )
     
+    val textureMap = mapOf(
+            0 to Texture("asfalt.png"),
+            1 to Texture("obrubnik.png")
+    
+    )
+
+    override fun tick(batch: SpriteBatch, deltaTime: Float) {
+        for ((x, row) in tileMap.withIndex()) {
+            for ((y, value) in row.withIndex()) {
+                batch.draw(textureMap[value] ?: textureMap[0], x.toFloat(), y.toFloat(), 1f, 1f)
+                
+            }
+            
+        }
+        
+    }
     
 }
