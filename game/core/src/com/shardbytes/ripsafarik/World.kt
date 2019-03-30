b7154f901242272882e98d8f37d98497ba2032e2
Plajdo <31418257+Plajdo@users.noreply.github.com>
2019-03-30 13:26:06 +0100

    lotsofstuff

--- game/core/src/com/shardbytes/ripsafarik/World.kt
+++ game/core/src/com/shardbytes/ripsafarik/World.kt
@@ -1,11 +1,11 @@
 package com.shardbytes.ripsafarik
 
 import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.Sprite
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
-import com.badlogic.gdx.graphics.g2d.TextureRegion
 
 class World : ITickable {
-    
+    /*
     val tileMap = arrayOf(
             arrayOf(4, 4, 0, 0, 0),
             arrayOf(4, 4, 0, 0, 0),
@@ -16,22 +16,33 @@ class World : ITickable {
             arrayOf(0, 1, 0, 1, 0, 1, 0, 1, 0, 1),
             arrayOf(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
             arrayOf(3, 3, 3, 3, 3, 3, 3, 3, 3, 3),
-            arrayOf(4, 4, 4, 4, 3, 3, 3, 4, 4, 4)
+            arrayOf(4, 4, 4, 4, 3, 3, 3, 5, 5, 4)
+    ).reversedArray()
+    */
+    
+    val tileMap = arrayOf(
+            arrayOf("0a", "0a", "0a", "0a", "0a"),
+            arrayOf("1a", "1a", "1a", "1a", "1a"),
+            arrayOf("0a", "0a", "4a", "4a", "5b"),
+            arrayOf("4a", "4a", "4a", "4a", "4a")
     ).reversedArray()
     
-    val textureMap = mapOf(
-            0 to Texture("asfalt.png"),
-            1 to Texture("asfalt_ciara.png"),
-            2 to Texture("obrubnik.png"),
-            3 to Texture("beton.png"),
-            4 to Texture("grass.png")
+    val spriteMap = mapOf(
+            "0a" to Sprite(Texture("grass.png")),
+            "1a" to Sprite(Texture("asfalt.png")),
+            "2a" to Sprite(Texture("asfalt_ciara.png")),
+            "3a" to Sprite(Texture("obrubnik.png")),
+            "4a" to Sprite(Texture("beton.png")),
+            "5a" to Sprite(Texture("stairs.png")).apply { setSize(1f, 1f); setOriginCenter()},
+            "5b" to Sprite(Texture("stairs.png")).apply { setSize(1f, 1f); setOriginCenter(); rotation = 90f }
     
     )
 
     override fun tick(batch: SpriteBatch, deltaTime: Float) {
         for ((y, row) in tileMap.withIndex()) {
             for ((x, value) in row.withIndex()) {
-                batch.draw(textureMap[value] ?: textureMap[0], x.toFloat(), y.toFloat(), 1f, 1f)
+                //batch.draw(textureMap[value] ?: textureMap[0], x.toFloat(), y.toFloat(), 1f, 1f)
+                spriteMap[value]!!.apply { setPosition(x.toFloat(), y.toFloat()) }.draw(batch)
                 
             }
             
