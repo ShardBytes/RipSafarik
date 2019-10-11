ea05b0d5303868f2b73d0f6b80ab0a576c4136aa
Plajdo <31418257+Plajdo@users.noreply.github.com>
2019-10-11 21:03:40 +0200

    reindent

--- game/core/src/com/shardbytes/ripsafarik/components/Entity.kt
+++ game/core/src/com/shardbytes/ripsafarik/components/Entity.kt
@@ -8,31 +8,31 @@ import com.badlogic.gdx.physics.box2d.Body
  * Represents a game object that can be located in the world as physical entity.
  */
 interface Entity : GameObject {
-	
-	/**
-	 * Physics box2d body.
-	 */
-	val body: Body
-	
-	/**
-	 * Gets position of the body
-	 */
-	override val position
-		get() = body.position
-	
-	/**
-	 * Sets the position of entity (sets its body transform)
-	 */
-	fun setPosition(x: Float, y: Float) {
-		body.setTransform(x, y, body.angle)
-	}
-	
-	/**
-	 * Sets and gets the angle of body in degrees.
-	 */
-	var rotation: Float
-		get() = body.angle * radDeg
-		set(degrees) {
-			body.setTransform(body.position, degrees * degRad)
-		}
+
+    /**
+     * Physics box2d body.
+     */
+    val body: Body
+
+    /**
+     * Gets position of the body
+     */
+    override val position
+        get() = body.position
+
+    /**
+     * Sets the position of entity (sets its body transform)
+     */
+    fun setPosition(x: Float, y: Float) {
+        body.setTransform(x, y, body.angle)
+    }
+
+    /**
+     * Sets and gets the angle of body in degrees.
+     */
+    var rotation: Float
+        get() = body.angle * radDeg
+        set(degrees) {
+            body.setTransform(body.position, degrees * degRad)
+        }
 }
