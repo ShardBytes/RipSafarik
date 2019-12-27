83680ab6009f9f7c10902317b4937d3c148b7347
Plajdo <31418257+Plajdo@users.noreply.github.com>
2019-12-27 23:56:29 +0100

    formatting v2

--- game/core/src/com/shardbytes/ripsafarik/ui/inventory/PlayerInventory.kt
+++ game/core/src/com/shardbytes/ripsafarik/ui/inventory/PlayerInventory.kt
@@ -16,93 +16,93 @@ import com.shardbytes.ripsafarik.ui.Healthbar
  * @see ItemInventory
  */
 object PlayerInventory {
-    
-    var floatingItem: Item? = null
 
-    var isOpened = false
-    val slots = createSlots()
+	var floatingItem: Item? = null
 
-    private val inventoryTexture = TextureRegion(Textures.UI["inventorybackground"])
+	var isOpened = false
+	val slots = createSlots()
 
-    fun render(dt: Float, batch: SpriteBatch) {
-        if(isOpened) {
-            drawBackground(batch)
-            drawSlots(batch)
-            
-            drawFloatingItem(batch)
+	private val inventoryTexture = TextureRegion(Textures.UI["inventorybackground"])
 
-        }
-        
-    }
-    
-    private fun drawFloatingItem(batch: SpriteBatch) {
-        val item = floatingItem
-        if(item != null) {
-            val mouseCoords = GameScreen.uiCamera.unproject(Gdx.input.x, Gdx.input.y)
-            
-            //item is drawn 1.1 unit big
-            batch.draw(item.texture,
-                    mouseCoords.x - 0.5f,
-                    mouseCoords.y - 0.5f,
-                    0f,
-                    0f,
-                    1f,
-                    1f,
-                    1.1f,
-                    1.1f,
-                    0f)
-            
-            if(item is IUsable) {
-                if(item.maxUses > 0) {
-                    batch.draw(Healthbar[item.leftUses],
-                            mouseCoords.x - 0.5f,
-                            mouseCoords.y - 0.5f,
-                            0f,
-                            0f,
-                            1f,
-                            0.05f,
-                            1.1f,
-                            1.1f,
-                            0f)
+	fun render(dt: Float, batch: SpriteBatch) {
+		if (isOpened) {
+			drawBackground(batch)
+			drawSlots(batch)
 
-                }
-                
-            }
-            
-        }
-        
-    }
+			drawFloatingItem(batch)
 
-    private fun drawBackground(batch: SpriteBatch) {
-        batch.draw(inventoryTexture,
-                0f - 1.5f, //  - width/2
-                0f - 1.5f, // - height/2
-                1.5f,
-                1.5f,
-                3f,
-                3f,
-                1f,
-                1f,
-                0f)
-    }
+		}
 
-    private fun drawSlots(batch: SpriteBatch) {
-        slots.forEach { it.render(batch) }
+	}
 
-    }
+	private fun drawFloatingItem(batch: SpriteBatch) {
+		val item = floatingItem
+		if (item != null) {
+			val mouseCoords = GameScreen.uiCamera.unproject(Gdx.input.x, Gdx.input.y)
 
-    private fun createSlots(): Array<ItemSlot> {
-        val slots = arrayListOf<ItemSlot>()
-        for (i in -1..1) {
-            for (j in -1..1) {
-                slots.add(ItemSlot().apply { screenPosition = Vector2(j.toFloat(), i.toFloat()) })
+			//item is drawn 1.1 unit big
+			batch.draw(item.texture,
+					mouseCoords.x - 0.5f,
+					mouseCoords.y - 0.5f,
+					0f,
+					0f,
+					1f,
+					1f,
+					1.1f,
+					1.1f,
+					0f)
 
-            }
+			if (item is IUsable) {
+				if (item.maxUses > 0) {
+					batch.draw(Healthbar[item.leftUses],
+							mouseCoords.x - 0.5f,
+							mouseCoords.y - 0.5f,
+							0f,
+							0f,
+							1f,
+							0.05f,
+							1.1f,
+							1.1f,
+							0f)
 
-        }
+				}
 
-        return slots.toArray(arrayOfNulls(9))
+			}
 
-    }
+		}
+
+	}
+
+	private fun drawBackground(batch: SpriteBatch) {
+		batch.draw(inventoryTexture,
+				0f - 1.5f, //  - width/2
+				0f - 1.5f, // - height/2
+				1.5f,
+				1.5f,
+				3f,
+				3f,
+				1f,
+				1f,
+				0f)
+	}
+
+	private fun drawSlots(batch: SpriteBatch) {
+		slots.forEach { it.render(batch) }
+
+	}
+
+	private fun createSlots(): Array<ItemSlot> {
+		val slots = arrayListOf<ItemSlot>()
+		for (i in -1..1) {
+			for (j in -1..1) {
+				slots.add(ItemSlot().apply { screenPosition = Vector2(j.toFloat(), i.toFloat()) })
+
+			}
+
+		}
+
+		return slots.toArray(arrayOfNulls(9))
+
+	}
 
 }
