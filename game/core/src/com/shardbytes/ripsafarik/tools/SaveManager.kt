09d5de9f55a78216b0f46581f72756612aa3925b
Plajdo <31418257+Plajdo@users.noreply.github.com>
2020-01-03 23:43:25 +0100

    comments

--- game/core/src/com/shardbytes/ripsafarik/tools/SaveManager.kt
+++ game/core/src/com/shardbytes/ripsafarik/tools/SaveManager.kt
@@ -2,60 +2,64 @@ package com.shardbytes.ripsafarik.tools
 
 import com.badlogic.gdx.Gdx
 import com.shardbytes.ripsafarik.Vector2Serializer
-import com.shardbytes.ripsafarik.assets.Textures
 import com.shardbytes.ripsafarik.components.world.Item
-import com.shardbytes.ripsafarik.entity.ItemDrop
 import com.shardbytes.ripsafarik.items.Flashlight
 import com.shardbytes.ripsafarik.items.Gun
 import com.shardbytes.ripsafarik.items.GunMagazine
 import com.shardbytes.ripsafarik.ui.inventory.Hotbar
-import com.shardbytes.ripsafarik.ui.inventory.ItemSlot
 import kotlinx.serialization.PolymorphicSerializer
-import kotlinx.serialization.Serializer
-import kotlinx.serialization.internal.ArrayListSerializer
-import kotlinx.serialization.internal.IntArraySerializer
-import kotlinx.serialization.internal.IntSerializer
-import kotlinx.serialization.internal.ReferenceArraySerializer
 import kotlinx.serialization.json.*
 import kotlinx.serialization.modules.SerializersModule
-import kotlinx.serialization.modules.plus
-import kotlinx.serialization.modules.serializersModule
-import kotlinx.serialization.modules.serializersModuleOf
-import kotlinx.serialization.serializer
-import kotlinx.serialization.stringify
-import java.util.ArrayList
 
 object SaveManager {
 
+	//save path
 	val savefile = Gdx.files.local("test.sav")
 
 	fun save() {
-		val polymorphicModule = SerializersModule { 
-			polymorphic(Item::class) { 
+		//specifies which classes can be (de)serialized with polymorphic serializer
+		val polymorphicModule = SerializersModule {
+			polymorphic(Item::class) {
+				//class - its serializer
+				//serializer is auto-generated when @Serializable annotation is found
 				Gun::class with Gun.serializer()
 				Flashlight::class with Flashlight.serializer()
 				GunMagazine::class with GunMagazine.serializer()
-				
-			}
-			
-		}
-		
-		val json = Json(JsonConfiguration.Stable, context = polymorphicModule)
 
-		val hotbarItems = arrayListOf<JsonElement>()
-		Hotbar.hotbarSlots.forEach {
-			val item = json.toJson(PolymorphicSerializer(Item::class), it.item ?: GunMagazine())
-			val position = json.toJson(Vector2Serializer, it.screenPosition)
-			
-			val jsonObj = JsonObject(mapOf("position" to position, "item" to item))
-			
-			hotbarItems.add(jsonObj)
-			
+			}
+
 		}
+
+		//json processor
+		val json = Json(JsonConfiguration.Stable, context = polymorphicModule)
 		
+		val hotbarItems = arrayListOf<JsonElement>()
+		
+		//loop over each itemslot
+		Hotbar.hotbarSlots.forEach {
+			//and use different serializers to serialize it
+			//eg. polymorphic (because Item can be any class specified above)
+			val item = json.toJson(PolymorphicSerializer(Item::class), it.item ?: GunMagazine())
+			
+			//or other specified serializers (IntSerializer, FloatSerializer, ...)
+			val position = json.toJson(Vector2Serializer, it.screenPosition)
+
+			//put it all together into one object
+			//add json tags so it's named somehow
+			val jsonObj = JsonObject(mapOf("position" to position, "item" to item))
+
+			//and add that slot into a map as an JSON object
+			hotbarItems.add(jsonObj)
+
+		}
+
+		//make an array of all the slots (which are now JSON objects)
+		//and put it to string
 		val string = JsonArray(hotbarItems).toString()
-		savefile.writeString(string, false, "UTF-8")
 		
+		//save JSON string to file
+		savefile.writeString(string, false, "UTF-8")
+
 	}
 
 }
