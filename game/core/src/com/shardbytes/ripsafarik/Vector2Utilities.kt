e05086e767377ce81129c41c3414903312cfb7cd
Plajdo <31418257+Plajdo@users.noreply.github.com>
2020-03-16 17:25:34 +0100

    Update Vector2Utilities.kt

--- game/core/src/com/shardbytes/ripsafarik/Vector2Utilities.kt
+++ game/core/src/com/shardbytes/ripsafarik/Vector2Utilities.kt
@@ -8,41 +8,41 @@ import kotlin.math.round
 import kotlin.math.roundToInt
 
 fun Vector2.inRange(start: Vector2, end: Vector2): Boolean {
-	if (this.x >= start.x && this.x <= end.x) {
-		if (this.y >= start.y && this.y <= end.y) {
-			return true
+    if (this.x >= start.x && this.x <= end.x) {
+        if (this.y >= start.y && this.y <= end.y) {
+            return true
 
-		}
+        }
 
-	}
-	return false
+    }
+    return false
 
 }
 
 fun Vector2.copyAndround(): Vector2 {
-	return Vector2(round(this.x), round(this.y))
+    return Vector2(round(this.x), round(this.y))
 
 }
 
 fun Vector2.identifier(): Long {
-	val INT_MASK = 0b0000000000000000000000000000000011111111111111111111111111111111L
+    val INT_MASK = 0b0000000000000000000000000000000011111111111111111111111111111111L
 
-	val xComponent = this.x.toRawBits().toLong() and INT_MASK
-	val yComponent = this.y.toRawBits().toLong() and INT_MASK
+    val xComponent = this.x.toRawBits().toLong() and INT_MASK
+    val yComponent = this.y.toRawBits().toLong() and INT_MASK
 
-	var id = 0b0000000000000000000000000000000000000000000000000000000000000000L
-	id = id or xComponent shl 32
-	id = id or yComponent
+    var id = 0b0000000000000000000000000000000000000000000000000000000000000000L
+    id = id or xComponent shl 32
+    id = id or yComponent
 
-	return id
+    return id
 
 }
 
 fun Long.toVector(): Vector2 {
-	val xComponent = Float.fromBits((this ushr 32).toInt())
-	val yComponent = Float.fromBits(toInt())
+    val xComponent = Float.fromBits((this ushr 32).toInt())
+    val yComponent = Float.fromBits(toInt())
 
-	return Vector2(xComponent, yComponent)
+    return Vector2(xComponent, yComponent)
 
 }
 
@@ -50,10 +50,10 @@ fun Long.vectorXComponent() = Float.fromBits((this ushr 32).toInt())
 fun Long.vectorYComponent() = Float.fromBits(toInt())
 
 fun Vector2.floor(): Vector2 {
-	x = floor(x)
-	y = floor(y)
+    x = floor(x)
+    y = floor(y)
 
-	return this
+    return this
 
 }
 
@@ -63,7 +63,7 @@ fun Vector2.floor(): Vector2 {
  * @return chunk coordinates
  */
 fun blockPositionToChunkCoordinates(blockPos: Vector2): Vector2 {
-	return blockPos.cpy().add(0.5f, 0.5f).scl(1f / 16f).floor()
+    return blockPos.cpy().add(0.5f, 0.5f).scl(1f / 16f).floor()
 
 }
 
@@ -74,55 +74,55 @@ fun blockPositionToChunkCoordinates(blockPos: Vector2): Vector2 {
  * @return block position inside chunk
  */
 fun blockPositionToChunkPosition(blockPos: Vector2): Vector2 {
-	return Vector2(
-			Math.floorMod(blockPos.x.roundToInt(), 16).toFloat(),
-			Math.floorMod(blockPos.y.roundToInt(), 16).toFloat()
-	)
-	
+    return Vector2(
+            Math.floorMod(blockPos.x.roundToInt(), 16).toFloat(),
+            Math.floorMod(blockPos.y.roundToInt(), 16).toFloat()
+    )
+
 }
 
 @Serializer(forClass = Vector2::class)
 object Vector2Serializer : KSerializer<Vector2> {
 
-	override val descriptor: SerialDescriptor = object : SerialClassDescImpl("Vector2") {
-		init {
-			addElement("x")
-			addElement("y")
+    override val descriptor: SerialDescriptor = object : SerialClassDescImpl("Vector2") {
+        init {
+            addElement("x")
+            addElement("y")
 
-		}
+        }
 
-	}
+    }
 
-	override fun serialize(encoder: Encoder, obj: Vector2) {
-		val compositeOutput = encoder.beginStructure(descriptor)
-		compositeOutput.encodeFloatElement(descriptor, 0, obj.x)
-		compositeOutput.encodeFloatElement(descriptor, 1, obj.y)
-		compositeOutput.endStructure(descriptor)
+    override fun serialize(encoder: Encoder, obj: Vector2) {
+        val compositeOutput = encoder.beginStructure(descriptor)
+        compositeOutput.encodeFloatElement(descriptor, 0, obj.x)
+        compositeOutput.encodeFloatElement(descriptor, 1, obj.y)
+        compositeOutput.endStructure(descriptor)
 
-	}
+    }
 
-	override fun deserialize(decoder: Decoder): Vector2 {
-		val decoder2 = decoder.beginStructure(descriptor)
-		var x: Float? = null
-		var y: Float? = null
+    override fun deserialize(decoder: Decoder): Vector2 {
+        val decoder2 = decoder.beginStructure(descriptor)
+        var x: Float? = null
+        var y: Float? = null
 
-		loop@ while (true) {
-			when (val i = decoder2.decodeElementIndex(descriptor)) {
-				CompositeDecoder.READ_DONE -> break@loop
-				0 -> x = decoder2.decodeFloatElement(descriptor, i)
-				1 -> y = decoder2.decodeFloatElement(descriptor, i)
-				else -> throw SerializationException("Unknown index $i")
+        loop@ while (true) {
+            when (val i = decoder2.decodeElementIndex(descriptor)) {
+                CompositeDecoder.READ_DONE -> break@loop
+                0 -> x = decoder2.decodeFloatElement(descriptor, i)
+                1 -> y = decoder2.decodeFloatElement(descriptor, i)
+                else -> throw SerializationException("Unknown index $i")
 
-			}
+            }
 
-		}
-		decoder2.endStructure(descriptor)
+        }
+        decoder2.endStructure(descriptor)
 
-		return Vector2(
-				x ?: throw MissingFieldException("x"),
-				y ?: throw MissingFieldException("y")
-		)
+        return Vector2(
+                x ?: throw MissingFieldException("x"),
+                y ?: throw MissingFieldException("y")
+        )
 
-	}
+    }
 
 }
